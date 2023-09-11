de6b5f4fa843c309f330ac0ebe7d3e0882dd346e
rpsobolewski <rpsobolewski@gmail.com>
2023-09-11 13:32:55 +0200

    added text

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            img:'https://picsum.photos/200/300'
+
+        }
+    },
+   
+
+
+}).mount('#app')
